f1a7e5a8e573c94ecd749089bc8046b467a42c86
0x74h51N <mr.tahsin@windowslive.com>
2025-08-09 17:50:46 +0300

    feat: diff-based mesh add/remove random mesh w/ uniform angle

--- src/loaders/utils/positioner.ts
+++ src/loaders/utils/positioner.ts
@@ -1,39 +1,59 @@
-import type { Mesh } from "three";
+import {
+  Vector3,
+  type Mesh,
+  type Object3D,
+  type Object3DEventMap,
+} from "three";
 import type { SpawnOptions } from "../types";
 
-type Positioner = (i: number, mesh: Mesh) => void;
+type Positioner = (mesh: Mesh) => void;
 
 // Returns a reusable positioning function that spaces meshes in a radial pattern,
 // avoiding overlap using a minimum distance threshold if provided.
 export function createPositioner(
   opts: SpawnOptions,
-  maxTry: number
+  maxTry: number,
+  existMeshes: Object3D<Object3DEventMap>[]
 ): Positioner {
   const placedPositions: [number, number][] = [];
-  const { count, radius, scale, minDistance, yPosition = 0 } = opts;
-  const nScale = scale.min + Math.random() * (scale.max - scale.min);
+  const tmp = new Vector3();
 
-  return (i: number, mesh: Mesh) => {
+  for (const obj of existMeshes) {
+    obj.getWorldPosition(tmp);
+    placedPositions.push([tmp.x, tmp.z]);
+  }
+
+  const { radius, scale, minDistance, yPosition = 0 } = opts;
+  const minD2 = minDistance * minDistance;
+  const rMin2 = radius.min * radius.min;
+  const rMax2 = radius.max * radius.max;
+
+  return (mesh: Mesh) => {
+    const nScale = scale.min + Math.random() * (scale.max - scale.min);
+
     mesh.scale.setScalar(nScale);
 
     mesh.position.set(0, 0, 0);
 
-    const angle = (i / count) * Math.PI * 2;
     let positionFound = false;
     let x = 0;
     let z = 0;
     let tryCount = 0;
 
     while (!positionFound && tryCount < maxTry) {
-      const nRadius = radius.min + Math.random() * (radius.max - radius.min);
-      x = Math.cos(angle) * nRadius;
-      z = Math.sin(angle) * nRadius;
+      const r2 = rMin2 + Math.random() * (rMax2 - rMin2);
+      const r = Math.sqrt(r2);
+
+      const theta = Math.random() * Math.PI * 2;
+
+      x = Math.cos(theta) * r;
+      z = Math.sin(theta) * r;
 
       if (minDistance > 0) {
         positionFound = placedPositions.every(([px, pz]) => {
-          const dx = x - px;
-          const dz = z - pz;
-          return dx * dx + dz * dz >= minDistance * minDistance;
+          const dx = x - px,
+            dz = z - pz;
+          return dx * dx + dz * dz >= minD2;
         });
       } else {
         positionFound = true;
--- src/loaders/utils/spawnMeshes.ts
+++ src/loaders/utils/spawnMeshes.ts
@@ -5,43 +5,50 @@ import { centerGeometryXZ, getRotation } from "./helpers";
 
 export function spawnMeshes({ manager, opts }: CountOpts) {
   const { group, baseMeshes } = manager;
+  const diff = opts.count - group.children.length;
 
-  // Dispose previous meshes in the group before spawn
-  group.children.forEach((child) => {
-    if (child instanceof Mesh) {
-      child.geometry?.dispose();
-      const mat = child.material;
-      if (Array.isArray(mat)) mat.forEach((m) => m.dispose());
-      else mat.dispose();
-    }
-  });
-  group.clear();
+  if (diff < 0) {
+    const disp = group.children.slice(diff);
 
-  const positioner = createPositioner(opts, 200);
-
-  // Randomly select and clone meshes from baseMeshes to the spawn count
-  for (let i = 0; i < opts.count; i++) {
-    const base = baseMeshes[Math.floor(Math.random() * baseMeshes.length)];
-    const mesh = base.clone(true);
-
-    mesh.traverse((child) => {
+    disp.forEach((child) => {
       if (child instanceof Mesh) {
-        child.castShadow = opts.castShadow;
-        child.receiveShadow = opts.receiveShadow;
-
-        opts.getGeoCenterXZ && centerGeometryXZ(child.geometry);
+        child.geometry?.dispose();
+        const mat = child.material;
+        if (Array.isArray(mat)) mat.forEach((m) => m.dispose());
+        else mat.dispose();
       }
+      group.remove(child);
     });
+  }
+  const existMeshes = group.children;
 
-    // Assign unique position and randomized scale to the mesh, according to spawn options
-    positioner(i, mesh);
+  if (diff > 0) {
+    const positioner = createPositioner(opts, 200, existMeshes);
 
-    //Rotation
-    const [x, y, z] = (["x", "y", "z"] as const).map(
-      (a) => getRotation(opts.rotation?.[a]) * Math.PI
-    );
-    mesh.rotation.set(x, y, z);
+    // Randomly select and clone meshes from baseMeshes to the spawn count
+    for (let i = 0; i < diff; i++) {
+      const base = baseMeshes[Math.floor(Math.random() * baseMeshes.length)];
+      const mesh = base.clone(true);
 
-    group.add(mesh);
+      mesh.traverse((child) => {
+        if (child instanceof Mesh) {
+          child.castShadow = opts.castShadow;
+          child.receiveShadow = opts.receiveShadow;
+
+          opts.getGeoCenterXZ && centerGeometryXZ(child.geometry);
+        }
+      });
+
+      // Assign unique position and randomized scale to the mesh, according to spawn options
+      positioner(mesh);
+
+      //Rotation
+      const [x, y, z] = (["x", "y", "z"] as const).map(
+        (a) => getRotation(opts.rotation?.[a]) * Math.PI
+      );
+      mesh.rotation.set(x, y, z);
+
+      group.add(mesh);
+    }
   }
 }
